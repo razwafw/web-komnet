c28e9aaa3960f47345cf0fa87065b4331fe9e060
Razwa Fazila Wibowo <r.razwafazilaw@gmail.com>
2024-03-07 14:12:30 +0700

    add font awesome to main template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,6 +10,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    {{-- Font Awesome --}}
+    <script src="https://kit.fontawesome.com/c880a1b0f6.js" crossorigin="anonymous"></script>
+
     <title>Halaman {{ $pageTitle }}</title>
 </head>
 
