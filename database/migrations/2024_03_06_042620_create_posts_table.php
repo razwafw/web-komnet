3cdff6cd4adac767e06aa808fe92a9744779d2f8
Razwa Fazila Wibowo <r.razwafazilaw@gmail.com>
2024-03-07 09:23:13 +0700

    use ulid for post id

--- database/migrations/2024_03_06_042620_create_posts_table.php
+++ database/migrations/2024_03_06_042620_create_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->ulid('id')->primary();
             $table->string('title');
             $table->string('description');
             $table->timestamps();
